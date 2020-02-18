c4109b526a37a4869a1eb9d306f7ac5da2ce0be9
Leonardo Tolosa <tolosa@gmail.com>
2020-02-18 15:32:00 -0300

    Migrate component five to function and hooks

--- src/components/Five.js
+++ src/components/Five.js
@@ -1,24 +1,21 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Little from "./Little";
 
-class Five extends Component {
-  state = {
-    showText: true
-  };
-  showLittle = () => {
-    this.setState({ showText: !this.state.showText });
+const Five = (props) => {
+  const [showText, setShowText] = useState(true);
+
+  const showLittle = () => {
+    setShowText(!showText);
   };
 
-  render() {
-    return (
-      <div style={{ marginBottom: "50px" }}>
-        <h2>Challenge 5</h2>
-        <h3>Here below lies little text in a box</h3>
-        <button onClick={this.showLittle}>Click to toggle Little</button>
-        {this.state.showText ? <Little /> : ""}
-      </div>
-    );
-  }
+  return (
+    <div style={{ marginBottom: "50px" }}>
+      <h2>Challenge 5</h2>
+      <h3>Here below lies little text in a box</h3>
+      <button onClick={showLittle}>Click to toggle Little</button>
+      {showText && <Little />}
+    </div>
+  );
 }
 
 export default Five;
