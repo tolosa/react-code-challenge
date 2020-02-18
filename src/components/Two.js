b77313cbeb9ec53a7b59cda9cc5983abee9f73b8
Leonardo Tolosa <tolosa@gmail.com>
2020-02-18 15:40:37 -0300

    Refactor component two to not use object

--- src/components/Two.js
+++ src/components/Two.js
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 
 const Two = (props) => {
-  const [user, setUser] = useState({ activeUser: 'Chris' });
+  const [activeUser, setActiveUser] = useState('Chris');
 
   const changeUser = () => {
-    setUser({ activeUser: 'Bolingo!' });
+    setActiveUser('Bolingo!');
   };
 
   return (
     <div style={{ marginBottom: "50px" }}>
       <h2>Challenge 2</h2>
-      <p>Active User is: {user.activeUser}</p>
+      <p>Active User is: {activeUser}</p>
       <button onClick={changeUser}>Change Me!</button>
     </div>
   );
