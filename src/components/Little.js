f341becfbbc5c1b607c3b2f5c6352c475d6a5f93
Leonardo Tolosa <tolosa@gmail.com>
2020-02-18 15:37:48 -0300

    Migrate component little to function and hooks

--- src/components/Little.js
+++ src/components/Little.js
@@ -1,17 +1,15 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 
-class Little extends Component {
-  componentWillUnmount() {
-    alert("Goodbye!!");
-  }
+const Little = (props) => {
+  useEffect(() =>
+    () => alert("Goodbye!!")
+  );
 
-  render() {
-    return (
-      <div style={{ marginBottom: "50px", border: "1px solid black" }}>
-        <h5> Hi I'm Little and its nice to meet you!!!</h5>
-      </div>
-    );
-  }
+  return (
+    <div style={{ marginBottom: "50px", border: "1px solid black" }}>
+      <h5> Hi I'm Little and its nice to meet you!!!</h5>
+    </div>
+  );
 }
 
 export default Little;
