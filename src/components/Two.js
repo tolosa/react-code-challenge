cdcbffcffc318202645b4fed70bcc2fa8439072c
Leonardo Tolosa <tolosa@gmail.com>
2020-02-18 15:13:41 -0300

    Migrate component two to function and hooks

--- src/components/Two.js
+++ src/components/Two.js
@@ -1,22 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Two extends Component {
-  state = {
-    activeUser: "Chris"
-  };
-  changeUser = () => {
-    this.setState({ activeUser: "Bolingo!" });
+const Two = (props) => {
+  const [user, setUser] = useState({ activeUser: 'Chris' });
+
+  const changeUser = () => {
+    setUser({ activeUser: 'Bolingo!' });
   };
 
-  render() {
-    return (
-      <div style={{ marginBottom: "50px" }}>
-        <h2>Challenge 2</h2>
-        <p>Active User is: {this.state.activeUser}</p>
-        <button onClick={this.changeUser}>Change Me!</button>
-      </div>
-    );
-  }
+  return (
+    <div style={{ marginBottom: "50px" }}>
+      <h2>Challenge 2</h2>
+      <p>Active User is: {user.activeUser}</p>
+      <button onClick={changeUser}>Change Me!</button>
+    </div>
+  );
 }
 
 export default Two;
