58336a3a9c584b8ba9ec2dede331a34365592f9d
Leonardo Tolosa <tolosa@gmail.com>
2020-02-18 15:18:14 -0300

    Migrate component three to function and hooks

--- src/components/Three.js
+++ src/components/Three.js
@@ -1,33 +1,32 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Three extends Component {
-  state = {
+const Three = (props) => {
+  const [car, setCar] = useState({
     year: 1995,
     type: "Mercedes",
     used: true
-  };
-  swapCar = () => {
-    this.setState({
+  });
+
+  const swapCar = () => {
+    setCar({
       year: 2018,
       type: "BMW",
       used: false
     });
   };
 
-  render() {
-    return (
-      <div style={{ marginBottom: "50px" }}>
-        <h2>Challenge 3</h2>
-        <h3>Car Spec is:</h3>
-        <ul>
-          <li>{this.state.type}</li>
-          <li>{this.state.year}</li>
-          <li>{this.state.used ? "Used Car" : "Brand New!"}</li>
-        </ul>
-        <button onClick={this.swapCar}>Swap Car!</button>
-      </div>
-    );
-  }
+  return (
+    <div style={{ marginBottom: "50px" }}>
+      <h2>Challenge 3</h2>
+      <h3>Car Spec is:</h3>
+      <ul>
+        <li>{car.type}</li>
+        <li>{car.year}</li>
+        <li>{car.used ? "Used Car" : "Brand New!"}</li>
+      </ul>
+      <button onClick={swapCar}>Swap Car!</button>
+    </div>
+  );
 }
 
 export default Three;
